de2f5319728ed38cf4860489e3c9492e9412b48e
Azuolas <magicgg.plays@gmail.com>
2024-11-07 08:24:58 +0200

    done?

--- main.py
+++ main.py
@@ -5,55 +5,55 @@ prekes = {
     "Gerimas": 1.50,
 }
 
-balansas=float(input('Iveskite suma, kuria norite isleisti: '))
+balansas=float(input('Įveskite sumą, kurią norite išleisti: €'))
 
 if balansas < 0.50:
     print("Jūsų įvestos sumos nepakanka jokiems daiktams įsigyti")
 else:
     if balansas < 0.75:
-        print('1. Saldainiai: 0.50')
+        print('1. Saldainiai: 0.50€')
     elif balansas < 1.00:
-        print('1. Saldainiai: 0.50')
-        print('2. Kramtomoji guma: 0.75')
+        print('1. Saldainiai: 0.50€')
+        print('2. Kramtomoji guma: 0.75€')
     elif balansas < 1.50:
-        print('1. Saldainiai: 0.50')
-        print('2. Kramtomoji guma: 0.75')
-        print('3. Traskuciai: 1.00')
+        print('1. Saldainiai: 0.50€')
+        print('2. Kramtomoji guma: 0.75€')
+        print('3. Traškučiai: 1.00€')
     else:
-        print('1. Saldainiai: 0.50')
-        print('2. Kramtomoji guma: 0.75')
-        print('3. Traskuciai: 1.00')
-        print('4. Gerimas: 1.50')
+        print('1. Saldainiai: 0.50€')
+        print('2. Kramtomoji guma: 0.75€')
+        print('3. Traškučiai: 1.00€')
+        print('4. Gėrimas: 1.50€')
 
     while True:
-        produktas = int(input("Iveskite produkto numeri "))
+        produktas = int(input("Įveskite produkto numerį: "))
 
         if produktas == 1 and balansas >= 0.50:
             likutis = balansas - 0.50
             print("Pasirinkote Saldainiai")
-            print("Dekojame uz jusu pirkini")
-            print(f"Jusu likutis yra {likutis:.2f}")
+            print("Dėkojame už jūsų pirkinį")
+            print(f"Jūsų likutis yra €{likutis:.2f}")
             break
         elif produktas == 2 and balansas >= 0.75:
             likutis = balansas - 0.75
             print("Pasirinkote Kramtomoji Guma")
-            print("Dekojame uz jusu pirkini")
-            print(f"Jusu likutis yra {likutis:.2f}")
+            print("Dėkojame už jūsų pirkinį")
+            print(f"Jūsų likutis yra €{likutis:.2f}")
             break
         elif produktas == 3 and balansas >= 1.00:
             likutis = balansas - 1.00
-            print("Pasirinkote Traskuciai")
-            print("Dekojame uz jusu pirkini")
-            print(f"Jusu likutis yra {likutis:.2f}")
+            print("Pasirinkote Traškučiai")
+            print("Dėkojame už jūsų pirkinį")
+            print(f"Jūsų likutis yra €{likutis:.2f}")
             break
         elif produktas == 4 and balansas >= 1.50:
             likutis = balansas - 1.50
-            print("Pasirinkote Gerimas")
-            print("Dekojame uz jusu pirkini")
-            print(f"Jusu likutis yra {likutis:.2f}")
+            print("Pasirinkote Gėrimas")
+            print("Dėkojame už jūsų pirkinį")
+            print(f"Jūsų likutis yra €{likutis:.2f}")
             break
         else:
-            print("Pasirinkote neteisinga produkta arba turite nepakankamai pinigu")
+            print("Pasirinkote neteisingą produkta arba turite nepakankamai pinigų")
 
 # match produktas:
 #     case "1":
